ac5e476bf64a83e2895a7b1ec1d58e49e816aa6c
minwoo <holiday@handong.ac.kr>
2023-09-09 19:26:37 +0900

    WordCRUD 생성

---
+++ src/main/java/org/example/WordCRUD.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class WordCRUD implements ICRUD {
+
+
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int select(int number) {
+        return 0;
+    }
+}
